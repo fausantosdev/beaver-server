3d8f20ad460d4f55f1948c0a5e0da66dbaaeb6d6
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2024-03-12 20:17:08 -0300

    chore: start server

---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+import { FastifyInstance } from 'fastify'
+
+export async function appRoutes (app: FastifyInstance) {
+  app.get('/', async (request, reply) => {
+    return reply.send({
+      teste: 'teste'
+    })
+  })
+}
