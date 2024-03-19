6b3508c3fc49dc951b42498208230efe236cc42d
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2024-03-18 22:30:50 -0300

    chore: records to-do list routes

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,9 +1,11 @@
 import { FastifyInstance } from 'fastify'
 
 import { userRoutes } from './user.routes'
+import { todoRoutes } from './todo.routes'
 
 const Routes = (app: FastifyInstance) => {
   app.register(userRoutes, { prefix: '/user' })
+  app.register(todoRoutes, { prefix: '/todo' })
 }
 
 export { Routes }
