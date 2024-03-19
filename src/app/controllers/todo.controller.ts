b9281229e9ef679cca2684392940e849b5e6819a
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2024-03-18 22:28:41 -0300

    chore: create the to-do lists controller

---
+++ src/app/controllers/todo.controller.ts
@@ -0,0 +1,118 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+import { TodoService } from '../services/todo.service'
+
+class TodoController {
+  private todoService: TodoService
+
+  constructor() {
+    this.todoService = new TodoService()
+  }
+
+  public store = async (request: FastifyRequest, reply: FastifyReply): Promise<FastifyReply> => {
+    const schema = z.object({
+      userId: z.string().uuid({ message: 'Incorrect ID format' }),
+      title: z.string().min(2, { message: 'Must be 2 or more characters long' })
+    })
+
+    try {
+      const { userId, title } = schema.parse(request.body)
+
+      const todo = await this.todoService.createTodo({ 
+        userId,
+        title
+      })
+  
+      return reply.send({
+        status: true,
+        data: todo,
+        message: null
+      })
+    } catch (error: any) {
+      return reply.send({
+        status: false,
+        data: null,
+        message: error.message
+      })
+    }
+  }
+
+  public readByUser = async (request: FastifyRequest, reply: FastifyReply): Promise<FastifyReply> => {
+    const schema = z.object({
+      userId: z.string().uuid({ message: 'Incorrect ID format' }),
+      id: z.string().uuid({ message: 'Incorrect ID format' }).optional()
+    })
+      
+    try {
+      const { userId, id } = schema.parse(request.params)
+
+      const todos = await this.todoService.getAllByUser(userId)
+      
+      return reply.send({
+        status: true,
+        data: todos,
+        message: null
+      })
+    } catch (error: any) {
+      return reply.send({
+        status: false,
+        data: null,
+        message: error.message
+      })
+    }
+  }
+
+  public update = async (request: FastifyRequest, reply: FastifyReply): Promise<FastifyReply> => {
+    const schema = z.object({
+      userId: z.string().uuid({ message: 'Incorrect ID format' }),
+      id: z.string().uuid({ message: 'Incorrect ID format' }),
+      title: z.string().min(2, { message: 'Must be 2 or more characters long' })
+    })
+    
+    try {
+      const { userId, id, title } = schema.parse(request.body)
+
+      const user = await this.todoService.edit(userId, id, { title })
+  
+      return reply.send({
+        status: true,
+        data: user,
+        message: null
+      })
+    } catch (error: any) {
+      return reply.send({
+        status: false,
+        data: null,
+        message: error.message
+      })
+    }
+  }
+
+  public delete = async (request: FastifyRequest, reply: FastifyReply): Promise<FastifyReply> => {
+    const schema = z.object({
+      userId: z.string().uuid({ message: 'Incorrect ID format' }),
+      id: z.string().uuid({ message: 'Incorrect ID format' })
+    })
+    
+    try {
+      const { userId, id } = schema.parse(request.body)
+
+      const result = await this.todoService.deleteOne(userId, id)
+  
+      return reply.send({
+        status: true,
+        data: result,
+        message: null
+      })
+    } catch (error: any) {
+      return reply.send({
+        status: false,
+        data: null,
+        message: error.message
+      })
+    }
+  }
+}
+
+export { TodoController }
