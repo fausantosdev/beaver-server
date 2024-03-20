984f22e0a055f01766c840783a95aadd3e685d6d
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2024-03-20 19:12:01 -0300

    feat: register authentication routes

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,8 +2,10 @@ import { FastifyInstance } from 'fastify'
 
 import { userRoutes } from './user.routes'
 import { todoRoutes } from './todo.routes'
+import { authRoutes } from './auth.routes' 
 
 const Routes = (app: FastifyInstance) => {
+  app.register(authRoutes, { prefix: '/auth' })
   app.register(userRoutes, { prefix: '/user' })
   app.register(todoRoutes, { prefix: '/todo' })
 }
