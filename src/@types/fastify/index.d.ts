ad8149533e59f5f6bb48220d7bb6a2d9bd760afe
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2024-03-20 16:13:28 -0300

    chore: add user object to fastify request with merge declaration

---
+++ src/@types/fastify/index.d.ts
@@ -0,0 +1,16 @@
+import 'fastify'
+
+declare module 'fastify' {
+  interface FastifyRequest {
+    user: {
+      id: string
+      username: string
+    },
+  }
+}
+
+/*
+ * Declaration merging
+ * 
+ * Font: https://g.co/gemini/share/e19ed8d1aa4a
+ */
