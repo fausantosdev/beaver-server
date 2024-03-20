b5e4ddfcad3551299ab748e0c9e2b7af93211bfb
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2024-03-20 19:30:33 -0300

    feat: add authentication plugin to routes

--- src/app/routes/user.routes.ts
+++ src/app/routes/user.routes.ts
@@ -2,14 +2,16 @@ import { FastifyInstance } from 'fastify'
 
 import { UserController } from '../controllers/user.controller'
 
+import { verifyToken } from '../plugins/auth.plugin'
+
 const userController = new UserController()
 
 export async function userRoutes (app: FastifyInstance) {
   app.post('/', userController.store)
 
-  app.get('/:id?', userController.read)
+  app.get('/:id?', { preHandler: [verifyToken] }, userController.read)
 
-  app.put('/:id?', userController.update)
+  app.put('/:id?', { preHandler: [verifyToken] }, userController.update)
 
-  app.delete('/:id?', userController.delete)
+  app.delete('/:id?', { preHandler: [verifyToken] }, userController.delete)
 }
