8335d774835a24a524e734858c4b2e38c567926e
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2024-03-26 11:13:21 -0300

    feat: add item routes

--- src/app/routes/todo.routes.ts
+++ src/app/routes/todo.routes.ts
@@ -1,14 +1,21 @@
 import { FastifyInstance } from 'fastify'
 
 import { TodoController } from '../controllers/todo.controller'
+import { ItemController } from '../controllers/item.controller'
 
-import { isAdmin, checkUserOrIsAdmin } from 'src/plugins/auth.plugin'
+import { verifyToken, checkUserOrIsAdmin } from 'src/plugins/auth.plugin'
 
 const todoController = new TodoController()
+const itemController = new ItemController()
 
 export async function todoRoutes (app: FastifyInstance) {
-  app.post('/', { preHandler: [checkUserOrIsAdmin] }, todoController.store) 
-  app.get('/:userId/:id?', { preHandler: [checkUserOrIsAdmin] }, todoController.readByUser)
-  app.patch('/', { preHandler: [checkUserOrIsAdmin] }, todoController.update)
-  app.delete('/', { preHandler: [checkUserOrIsAdmin] }, todoController.delete)
+  app.post('/', { preHandler: [verifyToken] }, todoController.store) 
+  app.get('/:userId/:id?', { preHandler: [verifyToken] }, todoController.readByUser)
+  app.patch('/', { preHandler: [verifyToken] }, todoController.update)
+  app.delete('/', { preHandler: [verifyToken] }, todoController.delete)
+
+  app.post('/item', { preHandler: [verifyToken] }, itemController.store)
+  app.get('/:todoId/items', { preHandler: [verifyToken] }, itemController.readByTodo) 
+  app.patch('/:todoId/item/:id/done', { preHandler: [verifyToken] }, itemController.doneItem)
+  app.delete('/:todoId/item/:id/delete', { preHandler: [verifyToken] }, itemController.delete)  
 }
