7c7d3501e20952948aa03565af70c5ba70cd74b8
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2024-03-26 11:12:16 -0300

    feat: create item resources

---
+++ src/app/controllers/item.controller.ts
@@ -0,0 +1,116 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+import { ItemService } from '../services/item.service' 
+
+class ItemController {
+  private itemService: ItemService
+
+  constructor() {
+    this.itemService = new ItemService()
+  }
+
+  public store = async (request: FastifyRequest, reply: FastifyReply): Promise<FastifyReply> => {
+    const schema = z.object({
+      todoId: z.string().uuid({ message: 'Incorrect ID format' }),
+      description: z.string().min(2, { message: 'Must be 2 or more characters long' })
+    })
+
+    try {
+      const { todoId, description } = schema.parse(request.body)
+
+      const todo = await this.itemService.createTodo({ 
+        todoId,
+        description
+      })
+  
+      return reply.send({
+        status: true,
+        data: todo,
+        message: null
+      })
+    } catch (error: any) {
+      return reply.send({
+        status: false,
+        data: null,
+        message: error.message
+      })
+    }
+  }
+
+  public readByTodo = async (request: FastifyRequest, reply: FastifyReply): Promise<FastifyReply> => {
+    const schema = z.object({
+      todoId: z.string().uuid({ message: 'Incorrect ID format' }),
+    })
+      
+    try {
+      const { todoId } = schema.parse(request.params)
+
+      const todos = await this.itemService.getAllByTodo(todoId)
+      
+      return reply.send({
+        status: true,
+        data: todos,
+        message: null
+      })
+    } catch (error: any) {
+      return reply.send({
+        status: false,
+        data: null,
+        message: error.message
+      })
+    }
+  }
+
+  public doneItem = async (request: FastifyRequest, reply: FastifyReply): Promise<FastifyReply> => {
+    const schema = z.object({
+      todoId: z.string().uuid({ message: 'Incorrect ID format' }),
+      id: z.string().uuid({ message: 'Incorrect ID format' })
+    })
+    
+    try {
+      const { todoId, id } = schema.parse(request.params)
+
+      const item = await this.itemService.done(todoId, id)
+  
+      return reply.send({
+        status: true,
+        data: item,
+        message: null
+      })
+    } catch (error: any) {
+      return reply.send({
+        status: false,
+        data: null,
+        message: error.message
+      })
+    }
+  }
+
+  public delete = async (request: FastifyRequest, reply: FastifyReply): Promise<FastifyReply> => {
+    const schema = z.object({
+      todoId: z.string().uuid({ message: 'Incorrect ID format' }),
+      id: z.string().uuid({ message: 'Incorrect ID format' })
+    })
+    
+    try {
+      const { todoId, id } = schema.parse(request.params)
+
+      const result = await this.itemService.deleteOne(todoId, id)
+  
+      return reply.send({
+        status: true,
+        data: result,
+        message: null
+      })
+    } catch (error: any) {
+      return reply.send({
+        status: false,
+        data: null,
+        message: error.message
+      })
+    }
+  }
+}
+
+export { ItemController }
---
+++ src/app/services/item.service.ts
@@ -0,0 +1,70 @@
+import { ItemRepository } from '../repositories/item.repository' 
+
+import { ItemDto, CreateItemDto } from '../dtos/todo.dtos'
+
+class ItemService {
+  private repository: ItemRepository
+
+  constructor() {
+    this.repository = new ItemRepository()
+  }
+
+  public createTodo = async ({ todoId, description }: CreateItemDto): Promise<ItemDto> => {
+    const newItem = await this.repository.create({ 
+      todoId,
+      description
+    })
+
+    return newItem
+  }
+
+  public getById = async (id: string) => {
+    return await this.repository.findOne({ id })
+  }
+
+  public getAllByTodo = async (todoId: string) => {
+    return await this.repository.read({ todoId })
+  }
+
+  public getMany = async (where = {}) => {
+    const result = await this.repository.read(where)
+
+    return result
+  }
+
+  public done = async (todoId: string, id: string) => {
+    const itemExists = await this.repository.findOne({ todoId, id })
+
+    if (!itemExists) throw new Error('Task list item not found')
+
+    const data = !itemExists.done ? { done: true } : { done: false }
+    
+    const updatedItem = this.repository.update({ todoId, id }, data)
+    
+    return updatedItem
+  }
+
+  public edit = async (todoId: string, id: string, data: object) => {
+    if (Object.keys(data).length === 0)  throw new Error('No data sent')
+
+    const todoItemExists = await this.repository.read({ todoId, id })
+
+    if (!todoItemExists) throw new Error('Task list item not found')
+
+    const result = await this.repository.update({ todoId, id }, data)
+
+    return result
+  }
+
+  public deleteOne = async (todoId: string, id: string) => {
+    const itemExists = await this.repository.read({ todoId, id })
+
+    if (itemExists.length == 0) throw new Error('Task list item not found')
+
+    const result = await this.repository.delete({ todoId, id })
+
+    return result
+  }
+}
+
+export { ItemService }
