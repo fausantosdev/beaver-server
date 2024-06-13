10a3879015316e93f07577c365f92606d6c28aeb
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2024-06-13 20:22:29 -0300

    feat: implements sign up route

--- src/app/routes/auth.routes.ts
+++ src/app/routes/auth.routes.ts
@@ -1,9 +1,12 @@
 import { FastifyInstance } from 'fastify'
 
 import { AuthController } from '../controllers/auth.controller'
+import { UserController } from '../controllers/user.controller'
 
 const authController = new AuthController()
+const userController = new UserController()
 
 export async function authRoutes (app: FastifyInstance) {
   app.post('/sign-in', authController.signIn)
+  app.post('/sign-up', userController.store)
 }
