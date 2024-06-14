fbd4e00df007a41304eca2c92a131b91020851d6
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2024-06-13 21:29:22 -0300

    chore: add typing

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -1,10 +1,10 @@
-import { sign, verify } from 'jsonwebtoken'
+import { JwtPayload, sign, verify } from 'jsonwebtoken'
 
-const generateToken = (payload: object) => {
+const generateToken = (payload: object): string => {
   return sign(payload, process.env.APP_KEY as string, { expiresIn: '1d' })
 }
 
-const decodeToken = (token: string) => {
+const decodeToken = (token: string): JwtPayload | string => {
   return verify(token, process.env.APP_KEY as string)
 }
 
