06896039ba952b571a3f52e1de7de6451025efda
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-02-05 21:45:41 -0300

    fix: make user ID parameter required

--- src/infra/http/routes/user.routes.ts
+++ src/infra/http/routes/user.routes.ts
@@ -9,7 +9,7 @@ const userControllers = createUserControllers()
 export async function userRoutes (app: FastifyInstance) {
   app.post('/', userControllers.create.handle)
 
-  app.get('/:id?', { preHandler: [checkUserOrIsAdmin] }, userControllers.get.handle)
+  app.get('/:id', { preHandler: [checkUserOrIsAdmin] }, userControllers.get.handle)
 
   app.put('/:id', { preHandler: [checkUserOrIsAdmin] }, userControllers.edit.handle)
 
