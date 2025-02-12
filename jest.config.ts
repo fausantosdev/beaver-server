538cb3c6da9696657a6ea657a7493f2d3c632d3d
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-02-12 12:31:08 -0300

    chore: update test coverage paths

--- jest.config.ts
+++ jest.config.ts
@@ -23,7 +23,7 @@ const config: Config = {
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts'
+    '<rootDir>/src/app/use-cases/**/*.ts'
   ],
 
   // The directory where Jest should output its coverage files
