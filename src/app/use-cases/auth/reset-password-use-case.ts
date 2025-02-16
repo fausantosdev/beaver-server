16caa948625c95c7b65df2d41c565f264b855f99
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-02-16 20:54:04 -0300

    refactor: replace generic Error with AppError

--- src/app/use-cases/auth/reset-password-use-case.ts
+++ src/app/use-cases/auth/reset-password-use-case.ts
@@ -1,5 +1,6 @@
 import { ResetPasswordDto } from '@dtos/auth-dtos'
 import { UserDto } from '@dtos/user.dtos'
+import { AppError } from '@errors/app-error'
 import { Encryption } from '@protocols/encryption'
 import { Repository } from '@protocols/repository'
 import { ResetPassword } from '@protocols/use-cases/auth/reset-password'
@@ -15,11 +16,11 @@ class ResetPasswordUseCase implements ResetPassword {
   async execute({ token, email, newPassword }: ResetPasswordDto): Promise<boolean> {
     const userExists = await this.userRepository.findOne({ email }) as UserDto
 
-    if (!userExists) throw new Error('E-mail not found')
+    if (!userExists) throw new AppError('E-mail not found', 401)
 
-    if(token !== userExists.password_reset_token) throw new Error('Invalid password recovery token')
+    if(token !== userExists.password_reset_token) throw new AppError('Invalid password recovery token', 401)
 
-    if (userExists.password_reset_expires && (new Date() > userExists.password_reset_expires)) throw new Error('Token expired, request a new one')
+    if (userExists.password_reset_expires && (new Date() > userExists.password_reset_expires)) throw new AppError('Token expired, request a new one', 401)
 
     const updated = await this.userRepository.update({
       id: userExists.id
@@ -29,7 +30,7 @@ class ResetPasswordUseCase implements ResetPassword {
       password_reset_expires: null
     })
 
-    if (!updated) throw new Error('An error occurred while updating your password, please try again')
+    if (!updated) throw new AppError('An error occurred while updating your password, please try again', 401)
 
     return true
   }
