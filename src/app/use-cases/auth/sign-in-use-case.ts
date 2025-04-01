dc78407293796ebdd071b2d28c7c26f046df6930
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-04-01 13:36:28 -0300

    fix: refactor** update path alias for lib to infra/services

--- src/app/use-cases/auth/sign-in-use-case.ts
+++ src/app/use-cases/auth/sign-in-use-case.ts
@@ -22,9 +22,9 @@ class SignInUseCase implements SignIn {
 
       if ( !user ) throw new AppError('Authentication failed, check your credentials', 401)
 
-      const { data: password_match } = await this.encryptionHelper.compare(password, user.password_hash)
+      const { status: encryptionStatus } = await this.encryptionHelper.compare(password, user.password_hash)
 
-      if ( user && !password_match ) throw new AppError('Authentication failed, check your credentials', 401)
+      if ( user && !encryptionStatus ) throw new AppError('Authentication failed, check your credentials', 401)
 
       const { status, data, message } = this.jwtHelper.generateToken({
         id: user.id,
