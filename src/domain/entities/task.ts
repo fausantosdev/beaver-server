316ac5cf1373d385f8a60681d8ff9069bd8959cb
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-06-02 20:43:38 -0300

    refactor: make User and Task properties required and add constructors

--- src/domain/entities/task.ts
+++ src/domain/entities/task.ts
@@ -1,11 +1,23 @@
 class Task {
   public readonly id?: string
   public parent_id?: string | null
-  public user_id?: string
-  public description?: string
+  public user_id: string
+  public description: string
   public progress?: number | null
   public readonly created_at?: Date
   public readonly updated_at?: Date
+
+  constructor({ parent_id, user_id, description, progress = 0 }: {
+    parent_id?: string | null
+    user_id: string
+    description: string
+    progress?: number
+  }) {
+    this.parent_id = parent_id || null
+    this.user_id = user_id
+    this.description = description
+    this.progress = progress
+  }
 }
 
 export { Task }
--- src/domain/entities/user.ts
+++ src/domain/entities/user.ts
@@ -1,13 +1,25 @@
 class User {
   public readonly id?: string
-  public name?: string
-  public email?: string
-  public password_hash?: string
+  public name: string
+  public email: string
+  public password_hash: string
   public role?: string
   public password_reset_token?: string | null
   public password_reset_expires?: Date | null
   public readonly created_at?: Date
   public readonly updated_at?: Date
+
+  constructor({ name, email, password_hash, role = 'user' }: {
+    name: string
+    email: string
+    password_hash: string
+    role?: string
+  }) {
+    this.name = name
+    this.email = email
+    this.password_hash = password_hash
+    this.role = role
+  }
 }
 
 export { User }
