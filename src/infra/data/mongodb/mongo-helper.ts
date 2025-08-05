7ba9bc877abd5921e3e0d58fe72fdf0f5fdcff6c
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-08-05 11:57:44 -0300

    feat: add connection control with isConnected flag and error handling

--- src/infra/data/mongodb/mongo-helper.ts
+++ src/infra/data/mongodb/mongo-helper.ts
@@ -1,20 +1,34 @@
 import { env } from '@config/env'
 import { mongoose } from '@config/mongo-client'
 
+let isConnected = false
+
 export const mongodb = {
-  connect: async () => {
-    if (mongoose.connection.readyState !== 1) {
-      await mongoose.connect(env.MONGO_URL, {})
+  connect: async (): Promise<void> => {
+    if (mongoose.connection.readyState !== 1 && !isConnected) {
+      try {
+        await mongoose.connect(env.MONGO_URL, {})
+        isConnected = true
+      } catch (error) {
+        console.error(`x failed to connect to mongodb: ${error}`)
+      } finally {
+        isConnected = false
+      }
     }
   },
 
-  disconnect: async () => {
-    if (mongoose.connection.readyState === 1) {
-      await mongoose.disconnect()
+  disconnect: async (): Promise<void> => {
+    if (mongoose.connection.readyState === 1 && isConnected) {
+      try {
+        await mongoose.disconnect()
+        isConnected = false
+      } catch (error) {
+        console.error(`x failed to disconnect from mongodb: ${error}`)
+      }
     }
   },
 
-  isConnected: () => {
+  isConnected: (): boolean => {
     return mongoose.connection.readyState === 1
   }
 }
